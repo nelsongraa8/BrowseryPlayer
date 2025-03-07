21c4d189102e239b3f922a694017fcbc820b722e
Nelson R. Graña <nelsongraa8@gmail.com>
2025-03-07 16:55:54 +0100

    feat: implementar carga de archivos y generación de miniaturas en el componente de video decoder

--- src/app/features/video_decoder/video_decoder.component.ts
+++ src/app/features/video_decoder/video_decoder.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 @Component({
 	selector: 'video-decoder',
@@ -8,4 +9,43 @@ import { Component } from '@angular/core';
 	templateUrl: 'video_decoder.component.html',
 	styleUrls: ['video_decoder.component.scss'],
 })
-export class VideoDecoderComponent {}
+export class VideoDecoderComponent implements OnInit {
+	ffmpeg = createFFmpeg({ log: true });
+
+	async ngOnInit() {
+		await this.ffmpeg.load();
+	}
+
+	async load(event: Event) {
+		const inputElement = event.target as HTMLInputElement;
+		if (!inputElement.files?.length) return;
+
+		const file = inputElement.files[0];
+		const fileName = file.name;
+
+		this.ffmpeg.FS('writeFile', fileName, await fetchFile(file));
+
+		await this.runFfmpeg(fileName);
+
+		this.getFileAndShow();
+	}
+
+	private async runFfmpeg(fileName: string) {
+		await this.ffmpeg.run(
+			'-i',
+			fileName,
+			'-frames:v',
+			'1',
+			'/tmp/thumbnail.jpg',
+		);
+	}
+
+	private getFileAndShow() {
+		const data = this.ffmpeg.FS('readFile', '/tmp/thumbnail.jpg');
+		const blob = new Blob([data.buffer], { type: 'image/jpeg' });
+		const url = URL.createObjectURL(blob);
+
+		const imgElement = document.getElementById('thumbnail') as HTMLImageElement;
+		imgElement.src = url;
+	}
+}
